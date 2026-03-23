a525225104e33fd473863a839eed189e5a1faae0
NadymB <dantrinh2792001@gmail.com>
2026-03-23 15:28:02 +0700

    fix consumer upload batch

--- etl/load_data.py
+++ etl/load_data.py
@@ -2,7 +2,7 @@ import asyncio
 from src.load.consumer import run
 
 def main():
-    asyncio.run(run())  
+    run()
 
 if __name__ == "__main__":
     main()
--- src/load/consumer.py
+++ src/load/consumer.py
@@ -1,5 +1,5 @@
 import time
-from src.utils.config import RESULT_SUCCESS_QUEUE, UPLOAD_PROCESSING_QUEUE, BATCH_SIZE, SLEEP_EMPTY
+from src.utils.config import RESULT_SUCCESS_QUEUE, UPLOAD_PROCESSING_QUEUE, BATCH_SIZE, SLEEP_EMPTY, CRAWL_QUEUE, PROCESSING_QUEUE
 from src.utils.logger import setup_logger
 from src.utils.redis_client import get_redis_sync
 from src.load.recover import recover_on_start
@@ -7,9 +7,19 @@ from src.load.queue_ops import pop_safe, remove_processed_jobs
 from src.load.gcs_loader import upload_batch 
 
 logger = setup_logger("upload_consumer")
+IDLE_TIMEOUT = 10 
+
+def is_system_done(r):
+    crawl = r.llen(CRAWL_QUEUE)
+    processing = r.llen(PROCESSING_QUEUE)
+    success = r.llen(RESULT_SUCCESS_QUEUE)
+
+    return crawl == 0 and processing == 0 and success == 0
+
 
 def run():
     logger.info("🚀 Upload consumer start")
+    logger.info(f"BATCH_SIZE = {BATCH_SIZE}")
     r = get_redis_sync()
     recover_on_start(r, RESULT_SUCCESS_QUEUE, UPLOAD_PROCESSING_QUEUE, logger)
 
@@ -20,7 +30,7 @@ def run():
         item = pop_safe(r, RESULT_SUCCESS_QUEUE, UPLOAD_PROCESSING_QUEUE)
 
         if item:
-            batch.append(item)
+            batch.append(item.decode())
 
         if len(batch) >= BATCH_SIZE:
             success = upload_batch(batch)
@@ -29,16 +39,23 @@ def run():
                 remove_processed_jobs(r, UPLOAD_PROCESSING_QUEUE, batch)
                 total += len(batch)
                 logger.info(f"📊 Total uploaded: {total}")
-                batch.clear()
+            batch.clear()
 
-        # queue rỗng → flush
         if not item:
-            if batch:
-                success = upload_batch(batch)
-                if success:
-                    remove_processed_jobs(r, UPLOAD_PROCESSING_QUEUE, batch)
-                    total += len(batch)
-                batch.clear()
+            if is_system_done(r):
+                if batch:
+                    logger.info(f"🏁 Final flush {len(batch)} items")
+
+                    success = upload_batch(batch)
+                    if success:
+                        remove_processed_jobs(r, UPLOAD_PROCESSING_QUEUE, batch)
+                        total += len(batch)
+
+                    batch.clear()
+
+                logger.info("😴 System idle... waiting for new data")
+            time.sleep(5)
+            continue
 
             time.sleep(SLEEP_EMPTY)
 
